3d29bbebd9cc5a00b04bcbe204031e085ad3bfa7
Igor <igoroshust75@gmail.com>
2024-11-12 16:15:22 +0900

    feat: add react-redux, App-redux.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom"; /* для рендеринга компонента */
 import 'bootstrap/dist/css/bootstrap.min.css'; /* стили для таблицы */
-import App from "./components/App"; /* импорт компонента */
-
+// import App from "./components/App"; /* импорт компонента */
+import ReduxApp from "./App-redux";
+import { Provider } from "react-redux";
 import { createStore } from "redux"; /* импорт функции для создания Store */
 
+
+const initialState = ["redux", "react"];
 const store = createStore(changeStore);
 
+
 /* Пишем функцию-редьюсер (изменяющую состояние) */
-function changeStore (state = [], action) {
-    console.log('1', state)
+function changeStore (state = initialState, action) {
     switch(action.type) {
         case 'WRITE': {
             return [
@@ -25,34 +28,39 @@ function changeStore (state = [], action) {
     }
 }
 
-/* Привязка к элементам */
-const testButton = document.querySelector(".testButton");
-const items = document.querySelector(".testUl")
+ReactDOM.render(
+<Provider store={store}>
+    <ReduxApp />
+</Provider>, document.getElementById('root'));
 
-/* Через subscribe подписываемся на изменения в Store */
-store.subscribe(() => {
-//     console.log('subscribe', store.getState())
+// /* Привязка к элементам */
+// const testButton = document.querySelector(".testButton");
+// const items = document.querySelector(".testUl")
 
-//     очищаем массив элементов при выводе
-    items.innerHTML = ''
-    const show = (document.querySelector(".testInput") as HTMLInputElement).value = ''
-
-//     выводим имеющиемся данные
-    store.getState().map(item => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        items.appendChild(li)
-    })
-})
-
-/* Метод dispatch изменяет события у store. Тип действия "запиши"
-Через dispatch инициируем действие и передаём action (write) -->
-В reducer'e рассмотрели тип "action"*/
-// store.dispatch({type: 'WRITE', payload: 'new message'})
-
-
-testButton.addEventListener('click', () => {
-    const inputValue = (document.querySelector(".testInput") as HTMLInputElement).value;
-    console.log('INPUT', inputValue)
-    store.dispatch({type: 'WRITE', payload: inputValue})
-});
+// /* Через subscribe подписываемся на изменения в Store */
+// store.subscribe(() => {
+// //     console.log('subscribe', store.getState())
+//
+// //     очищаем массив элементов при выводе
+//     items.innerHTML = ''
+//     const show = (document.querySelector(".testInput") as HTMLInputElement).value = ''
+//
+// //     выводим имеющиемся данные
+//     store.getState().map(item => {
+//         const li = document.createElement("li");
+//         li.textContent = item;
+//         items.appendChild(li)
+//     })
+// })
+//
+// /* Метод dispatch изменяет события у store. Тип действия "запиши"
+// Через dispatch инициируем действие и передаём action (write) -->
+// В reducer'e рассмотрели тип "action"*/
+// // store.dispatch({type: 'WRITE', payload: 'new message'})
+//
+//
+// testButton.addEventListener('click', () => {
+//     const inputValue = (document.querySelector(".testInput") as HTMLInputElement).value;
+//     console.log('INPUT', inputValue)
+//     store.dispatch({type: 'WRITE', payload: inputValue})
+// });
